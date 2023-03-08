98d89874ee94c6950c84461464542a2fd102ae45
VitongoviskDev <v_campos_dev@outlook.com>
2023-03-08 20:27:20 -0300

    Initial dist subtree commit

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import './index.css';
 import { OpenPage } from "./Pages/Controllers/PagesController";
 import MainMenu from "./Pages/MainMenuPage";
 
+localStorage.nickname = JSON.stringify(null);
+localStorage.currentPageContainerId = JSON.stringify(null);
+localStorage.currentFormId = JSON.stringify(null);
+localStorage.nickname = JSON.stringify(null);
+localStorage.winner = JSON.stringify(null);
+localStorage.playerGameBoard = JSON.stringify(null);
+localStorage.machineGameboard = JSON.stringify(null);
+localStorage.selectedShip = JSON.stringify(null);
+
 OpenPage(MainMenu().id);
